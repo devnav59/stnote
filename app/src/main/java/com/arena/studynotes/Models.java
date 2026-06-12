88a87345330335a2cb1c09ee5b1faea534a1daa7
devnav59 <razmjoo59@gmail.com>
2026-06-12 11:40:22 +0330

    start

---
+++ app/src/main/java/com/arena/studynotes/Models.java
@@ -0,0 +1,2 @@
+package com.arena.studynotes;
+class Topic{long id;String title,desc;int color;int count;} class Note{long id,topicId;String title,chapter,body,pdfName,pdfUri;int pdfPage;boolean tip;long created,updated;}
